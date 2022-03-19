046b7a3b372c9449952daf987958f8460b6bce96
Kieran Finn <kieran.finn@hotmail.com>
2022-03-19 13:20:02 +0000

    fixed emails

--- send_emails.py
+++ send_emails.py
@@ -4,10 +4,11 @@ from config import *
 import os
 
 
-def send_async_email(message):
+def send_async_email(subject, body):
         # Replace the number with your own, or consider using an argument\dict for multiple people.
 	#to_email='kieran.finn@hotmail.com'
 	auth = ('kieran.errors@gmail.com', os.environ['GMAIL_PASSWORD'])
+	message=message = 'Subject: {}\n\n{}'.format(subject, body)
 
 	# Establish a secure session with gmail's outgoing SMTP server using your gmail account
 	try:
@@ -24,18 +25,4 @@ def send_async_email(message):
 	return 0
 
 def send_email(subject,body): #todo add subject to email
-	Thread(target=send_async_email(body)).start()
-
-
-# def send_email(message):
-#         # Replace the number with your own, or consider using an argument\dict for multiple people.
-# 	to_email='kieran.finn@hotmail.com'
-# 	auth = ('kieran.errors@gmail.com', os.environ['GMAIL_PASSWORD'])
-# 	server = smtplib.SMTP( "smtp.gmail.com", 587 )
-# 	server.starttls()
-# 	server.login(auth[0], auth[1])
-
-# 	# Send text message through SMS gateway of destination number
-# 	server.sendmail( auth[0], to_email, message)
-
-# 	return 0
+	Thread(target=send_async_email,args=(subject,body)).start()
